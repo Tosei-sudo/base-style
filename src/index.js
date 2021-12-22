c8f421bbd83f957e4a0bef295677e58ea97a0935
Tosei-sudo <tosei.sudo@gmail.com>
2021-12-22 09:42:17 +0900

    create scss file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import linenotify from './app';
-
-Vue.component('line-notify', linenotify);
+import "./scss/Foundation/reset.scss";
+import "./scss/Foundation/base.scss";
+import "./scss/Layout/main.scss";
+import "./scss/Layout/footer.scss";
+import "./scss/Layout/header.scss";
+import "./scss/Object/Project/p-humMenu.scss";
+import "./scss/Object/Project/p-icon.scss";
+import "./scss/Object/Project/p-InformationArea.scss";
+import "./scss/Object/Project/p-BottomNavi.scss";
+import "./scss/Object/Project/p-Card.scss";
+import "./scss/Object/Utility/u-utility.scss";
+import "./scss/Object/Utility/u-margin.scss";
+import "./scss/Object/Utility/u-padding.scss";
+import "./scss/Object/Utility/u-text.scss";
+import "./scss/Object/Utility/u-height.scss";
+import "./scss/Object/Utility/u-width.scss";
